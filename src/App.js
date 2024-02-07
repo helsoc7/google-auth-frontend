795ea3273c3a63623a01c29d93a6d02c15ae8c4e
Helen Haveloh <h.havelohh@gmail.com>
2024-02-07 22:31:28 +0100

    frontend dance

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import GoogleSignInButton from './components/GoogleSignInButton';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleSignInButton />
+    </div>
+  );
+}
+
+
+
+export default App;
